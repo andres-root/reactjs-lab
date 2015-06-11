eea6a10e7efa29c2548209e1b74b681a8c8e99b6
andres-root <andres.root.coder@gmail.com>
2015-06-11 16:43:50 -0500

    adding ajax data

--- content/js/app.js
+++ content/js/app.js
@@ -13,7 +13,7 @@ var CommentBox = React.createClass({
 				this.setState( { data: data } );
 			}.bind( this ),
 			error: function ( xhr, status, err ) {
-				console.error( xhrthis.props.url, status. err.toString() );
+				console.error( this.props.url, status, err.toString() );
 			}.bind( this )
 		});
 	},
